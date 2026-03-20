e74e5cedd9dfd03ae4f9a1089f110133c537753b
V-Lozovoy <basarabvova2015@gmail.com>
2026-03-20 14:11:50 +0200

    Initial repository added

--- src/main/java/com/example/catalog/entities/Product.java
+++ src/main/java/com/example/catalog/entities/Product.java
@@ -1,11 +1,22 @@
 package com.example.catalog.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.math.BigDecimal;
 
 // TODO: use lombok
 
+@Entity
+@Table(name = "products")
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  
   private String name;
   private String description;
   private BigDecimal price;
